d8ac215db712e1ced5541892a4b19721b4d056d4
yancao <caoyanc@sfu.ca>
2017-02-13 17:14:08 -0800

    start to add help, option, main

--- app/src/main/java/com/sfu276assg1/yancao/mineseeker/MenuActivity.java
+++ app/src/main/java/com/sfu276assg1/yancao/mineseeker/MenuActivity.java
@@ -1,7 +1,10 @@
 package com.sfu276assg1.yancao.mineseeker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,6 +12,37 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        launchPlayBtn();
+        launchOptionBtn();
+        launchHelpBtn();
 
+    }
+    private void launchPlayBtn() {
+        Button btn = (Button) findViewById(R.id.btn_play);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+
+    private void launchOptionBtn() {
+        Button btn = (Button) findViewById(R.id.btn_option);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),OptionActivity.class));
+            }
+        });
+    }
+    private void launchHelpBtn() {
+        Button btn = (Button) findViewById(R.id.btn_help);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),HelpActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/sfu276assg1/yancao/mineseeker/OptionActivity.java
@@ -0,0 +1,13 @@
+package com.sfu276assg1.yancao.mineseeker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+    }
+}
